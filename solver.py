aceb9109f0d5c262e98b37257ba26f0b5c6959ee
clodman84 <cshourin@gmail.com>
2025-01-15 13:28:33 +0530

    found all solutions

--- solver.py
+++ solver.py
@@ -5,8 +5,12 @@ from polyomino.board import Rectangle
 
 polyominos, board = read_puzzle(Path('./puzzle.txt'))
 cover_size = sum(len(poly) for poly in polyominos)
-print(cover_size)
+if cover_size < len(board):
+    polyominos += [[(0, 0)]] * (len(board) - cover_size)
 
-solution = Rectangle(x=7, y=6).tile_with(polyominos).solve()
-with open("solution.png", "wb") as file:
-    file.write(solution_to_png(solution))
+solutions = Rectangle(x=7, y=6).tile_with(polyominos).get_all_solutions()
+
+for i, solution in enumerate(solutions):
+    print(f"writing solution {i}")
+    with open(f"solution_{i}.png", "wb") as file:
+        file.write(solution_to_png(solution))
